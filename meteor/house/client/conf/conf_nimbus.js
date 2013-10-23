fbf29df05120a8d4de023b5350a9ecc1051deef5
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-10-23 09:15:31 +0100

    Add Nimbusbase

---
+++ meteor/house/client/conf/conf_nimbus.js
@@ -0,0 +1,10 @@
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+    var sync_string;
+
+    sync_string = "eyJHRHJpdmUiOnsia2V5IjoiODY1NzY0Mzc4NTkwLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic2NvcGUiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9hdXRoL2RyaXZlIiwiYXBwX25hbWUiOiJIb3VzZVNjcmFwIn0sIkRyb3Bib3giOnsia2V5IjoiYzdxeDZuaGoycWt5NGx5Iiwic2VjcmV0IjoicmtlMHNmOGpxdnJrOTgxIiwiYXBwX25hbWUiOiJIb3VzZVNjcmFwIn19"; 
+
+    Nimbus.Auth.setup(sync_string);
+    console.log("Nimbus setuped");
+  });
+}
---
+++ meteor/house/client/lib/user_data_store.js
@@ -0,0 +1,57 @@
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+
+    HousesPreferences = Nimbus.Model.setup("Houses", ["desc_hash"]);
+
+
+    Nimbus.Auth.authorized_callback = function() {
+      HousesPreferences = Nimbus.Model.setup("Houses", ["desc_hash"]);
+      console.log("authentication finished o/");
+
+      houses_to_remove = [];
+      HousesPreferences.each(function(house){
+        houses_to_remove.push(house.desc_hash);
+      });
+
+      Houses.find({}).forEach(function(house){
+        to_remove = houses_to_remove.indexOf(house.desc_hash);
+        console.log(to_remove);
+        if (to_remove >= 0) {
+          console.log("it has to be removed");
+          house.removeFromMap();
+        }
+      });
+
+    }
+
+    log_out = function() {
+      Nimbus.Auth.logout();
+      console.log("Logged Out");
+    };
+
+    connect_dropbox = function() {
+      Nimbus.Auth.authorize('Dropbox');
+      console.log("Connect Dropbox....");
+    };
+    
+    connect_google = function() {
+      Nimbus.Auth.authorize('GDrive');
+      console.log("Connect Google....");
+    };
+
+    not_interesting = function(desc_hash) {
+      if (Nimbus.Auth.authorized()){
+        if (! HousesPreferences.findByAttribute("desc_hash", desc_hash)){
+          HousesPreferences.create({"desc_hash": desc_hash});
+          console.log(desc_hash + ' should be removed');
+          Houses.findOne({desc_hash: desc_hash}).removeFromMap();
+          $('#house').html('');
+        }else {
+          console.log("House already deleted");
+        }
+      }else {
+        alert("Please log-in!");
+      }
+    };
+  });
+}
