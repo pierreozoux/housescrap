3f4e63cb9c1d2f0cbb3877141cfff26234140b65
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-03 15:11:49 +0000

    Try to scrap with meteor live production directly

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-scrapy crawl sapo
+#!/bin/bash -eux
+
+# cd ./meteor/house
+# url=`meteor mongo --url`
+# mongo_server=`echo $url | cut -d/ -f 3`
+# mongo_db=`echo $url | cut -d/ -f 4`
+# mongo_uri="mongodb://$mongo_server"
+
+# cd ../..
+# #-s MONGODB_PORT=$mongo_port 
+# echo "db = $mongo_db"
+# echo "uri = $mongo_uri"
+# scrapy crawl sapo -s MONGODB_URI=$mongo_uri -s MONGO_DATABASE=$mongo_db
+# scrapy crawl sapo
 scrapy crawl custojusto
