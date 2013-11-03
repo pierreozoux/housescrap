8a9635288ea741d1ca283a8f1fc7074b543b05ad
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-03 18:22:58 +0000

    Stop the spider if the date is older than a month

--- house/spiders/custojusto_spider.py
+++ house/spiders/custojusto_spider.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from scrapy.exceptions import CloseSpider
 from house.items import HouseItem
 from urlparse import urljoin
 from scrapy.http import Request
+import re
+import datetime
+from scrapy import log
 import json
 import urllib2
 import urllib
 import urlparse
 import httplib2
+import locale
 
 class CustojustoSpider(BaseSpider):
   name = "custojusto"
@@ -23,11 +28,11 @@ class CustojustoSpider(BaseSpider):
     for house in houses:
       link = house.select('div/a/@href').extract()[0]
       yield Request(link,callback=self.parseHouse)
-  
+    
     try:
       next_page = hxs.select('//div[contains(@id, "content_footer")]/div/div/*[contains(text(),"Seguinte")]/@href').extract()[0]
     except:
-      print "End page"
+      log.msg("End page", level=log.INFO)
     else:
       next_url = urljoin(response.url,next_page)
       # uncomment the next one if you want to scrap more than the first page
@@ -52,7 +57,22 @@ class CustojustoSpider(BaseSpider):
     item['desc'] = hxs.select('//div[contains(@class, "body_text")]/text()').extract()
 
     item['price'] = hxs.select('//span[contains(@class, "coolprice")]/text()').extract()[0].strip()
+
+    locale.setlocale(locale.LC_ALL, 'pt_PT')
     item['publication'] = hxs.select('//p[contains(@class,"right")]/text()').extract()[1].strip()
+    if re.match("Ontem|Hoje", item['publication']) is None:
+      log.msg("date is older, analyse...", level=log.INFO)
+      computed_date = datetime.datetime.strptime(item['publication'], "%d %b %H:%M")
+      if datetime.date.today().month is 1 and computed_date.month is 12:
+        year = (datetime.date.today() - datetime.timedelta(days=365)).year
+      else:
+        year = datetime.date.today().year
+
+      computed_date = datetime.datetime.strptime(item['publication'] + ' %d' % year, "%d %b %H:%M %Y")
+      one_month_ago = datetime.datetime.today() - datetime.timedelta(days=30)
+      if computed_date < one_month_ago:
+        log.msg("Too old...", level=log.INFO)
+        raise CloseSpider('Houses are too old')
 
     image_from_script = hxs.select('//div[contains(@id, "slider")]/script/text()').extract()
     images_urls = image_from_script[0].split('[')[1].split(',')
