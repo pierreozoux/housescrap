758670a8ea007607ebea3ffda563ef684ca49ba5
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-03 18:21:50 +0000

    Corect price pub

--- house/pipelines.py
+++ house/pipelines.py
@@ -27,7 +27,7 @@ class TitlePipeline(object):
 
 class PricePipeline(object):
   def process_item(self, item, spider):
-    price = item['price'].replace(u"€","").strip()
+    price = item['price'].replace(u"€","").strip().replace(" ", "")
 
     item['price'] = int(price)
     return item
