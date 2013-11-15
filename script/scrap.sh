162de6f06062953a772cd6bc771cfa1474a28c7c
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-15 00:13:23 +0000

    tried again...

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -eux
 
 # cd ./meteor/house
-# url=`meteor mongo --url`
+# url=`meteor mongo --url housescrap2.meteor.com`
 # mongo_server=`echo $url | cut -d/ -f 3`
 # mongo_db=`echo $url | cut -d/ -f 4`
 # mongo_uri="mongodb://$mongo_server"
-
 # cd ../..
+
 # #-s MONGODB_PORT=$mongo_port 
 # echo "db = $mongo_db"
 # echo "uri = $mongo_uri"
-# scrapy crawl sapo -s MONGODB_URI=$mongo_uri -s MONGO_DATABASE=$mongo_db
-# scrapy crawl sapo
+
+# scrapy crawl custojusto -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
+# scrapy crawl sapo -s MONGODB_URI=mongodb://127.0.0.1:3002/ -s MONGODB_DATABASE=meteor
+scrapy crawl sapo
 scrapy crawl custojusto
