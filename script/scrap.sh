0cd2b95e22df9738c606610fef3cdab886564178
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-15 21:08:38 +0000

    Finally a nice script

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,17 +1,14 @@
 #!/bin/bash -eux
 
-# cd ./meteor/house
-# url=`meteor mongo --url housescrap2.meteor.com`
-# mongo_server=`echo $url | cut -d/ -f 3`
-# mongo_db=`echo $url | cut -d/ -f 4`
-# mongo_uri="mongodb://$mongo_server"
-# cd ../..
+read -s -p "Enter Password: " password
 
-# #-s MONGODB_PORT=$mongo_port 
-# echo "db = $mongo_db"
-# echo "uri = $mongo_uri"
+cd ./meteor/house
+cmd=$(meteor mongo --url housescrap3.meteor.com << ENDPASS
+$password
+ENDPASS)
+mongo_uri=$(echo $cmd | cut -f2 -d" ")
+mongo_db=`echo $mongo_uri | cut -d/ -f 4`
+cd ../..
 
-# scrapy crawl custojusto -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
-# scrapy crawl sapo -s MONGODB_URI=mongodb://127.0.0.1:3002/ -s MONGODB_DATABASE=meteor
-scrapy crawl sapo
-scrapy crawl custojusto
+scrapy crawl custojusto -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
+# scrapy crawl sapo -s MONGODB_URI=$url -s MONGODB_DATABASE=$mongo_db
