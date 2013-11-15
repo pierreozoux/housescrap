cf1505f792d7263c21f21ee18d255eb813acd55b
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-11-15 00:36:46 +0000

    Update Gdrive key

--- meteor/house/client/conf/conf_nimbus.js
+++ meteor/house/client/conf/conf_nimbus.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
     Meteor.startup(function () {
     var sync_string;
 
-    sync_string = "eyJHRHJpdmUiOnsia2V5IjoiODY1NzY0Mzc4NTkwLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic2NvcGUiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9hdXRoL2RyaXZlIiwiYXBwX25hbWUiOiJIb3VzZVNjcmFwIn0sIkRyb3Bib3giOnsia2V5IjoiYzdxeDZuaGoycWt5NGx5Iiwic2VjcmV0IjoicmtlMHNmOGpxdnJrOTgxIiwiYXBwX25hbWUiOiJIb3VzZVNjcmFwIn19"; 
+    sync_string = "eyJHRHJpdmUiOnsia2V5IjoiODY1NzY0Mzc4NTkwLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic2NvcGUiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9hdXRoL2RyaXZlIiwiYXBwX25hbWUiOiJIb3VzZVNjcmFwIn0sIkRyb3Bib3giOnsia2V5IjoiIiwic2VjcmV0IjoiIiwiYXBwX25hbWUiOiIifX0"; 
 
     Nimbus.Auth.setup(sync_string);
     console.log("Nimbus setuped");
