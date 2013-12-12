3d56ae5c15b6380b305583f686bd31e68785a170
Pierre Ozoux <pierre.ozoux@gmail.com>
2013-12-12 19:32:19 +0000

    Make the script comºatible for raspberry \o/

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -eux
 
-read -s -p "Enter Password: " password
+password=`cat ~/.pass`
 
-cd ./meteor/house
-cmd=$(meteor mongo --url housescrap3.meteor.com << ENDPASS
-$password
-ENDPASS)
+cmd=$(/opt/node22/bin/node "/opt/meteor/tools/meteor.js" mongo --url findahouse.meteor.com <<<$password)
 mongo_uri=$(echo $cmd | cut -f2 -d" ")
 mongo_db=`echo $mongo_uri | cut -d/ -f 4`
-cd ../..
 
 scrapy crawl custojusto -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
-# scrapy crawl sapo -s MONGODB_URI=$url -s MONGODB_DATABASE=$mongo_db
+
+cmd=$(/opt/node22/bin/node "/opt/meteor/tools/meteor.js" mongo --url findahouse.meteor.com <<<$password)
+mongo_uri=$(echo $cmd | cut -f2 -d" ")
+mongo_db=`echo $mongo_uri | cut -d/ -f 4`
+
+scrapy crawl sapo -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
