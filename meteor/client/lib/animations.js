e9c79097acd651de4e7b85594e477b4dc7cbe4d7
Pierre Ozoux <pierre.ozoux@gmail.com>
2014-03-02 15:00:19 +0000

    Add about div

--- meteor/client/lib/animations.js
+++ meteor/client/lib/animations.js
@@ -16,6 +16,25 @@ if (Meteor.isClient) {
     document.getElementById('background-popup').style.opacity = 1;
   };
 
+  hideAbout = function() {
+    console.log("ovubhreoin");
+    hideBackground();
+    document.getElementById('about').style.bottom = -120 + "px";
+    $('background-popup').off( "click", hideAbout);
+    $('#open-about').off( "click", hideAbout);
+    var rotate = "rotate(0turn)";
+    document.getElementById('chevron-about').style.webkitTransform = rotate;
+  };
+
+  showAbout = function() {
+    showBackground();
+    document.getElementById('about').style.bottom = 200 + "px";
+    $('#background-popup').on( "click", hideAbout);
+    $('#open-about').on( "click", hideAbout);
+    var rotate = "rotate(0.5turn)";
+    document.getElementById('chevron-about').style.webkitTransform = rotate;
+  };
+
   $( window ).resize(function() {
     // resize map
     var height = window.innerHeight;
@@ -26,4 +45,9 @@ if (Meteor.isClient) {
     // put the form div in the center
     document.getElementById("form").style.left = ((width - 460)/2) + "px";
     document.getElementById("data-store").style.left = ((width - 400)/2) + "px";
+
+    // put the about div in the center
+    document.getElementById("about").style.left = ((width - 400)/2) + "px";
+    document.getElementById("about-button").style.left = ((width - 100)/2) + "px";
+  });
 }
