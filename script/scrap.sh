13e45d31390afcd915010a154bb5b3350ccf6342
Pierre Ozoux <pierre.ozoux@gmail.com>
2014-03-02 15:00:54 +0000

    New meteor mongo...

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -eux
 
-password=`cat ~/.pass`
-
-cmd=$(/opt/node22/bin/node "/opt/meteor/tools/meteor.js" mongo --url findahouse.meteor.com <<<$password)
+cmd=$(meteor mongo --url findahouse)
 mongo_uri=$(echo $cmd | cut -f2 -d" ")
 mongo_db=`echo $mongo_uri | cut -d/ -f 4`
 
 scrapy crawl custojusto -s MONGODB_URI=$mongo_uri -s MONGODB_DATABASE=$mongo_db
 
-cmd=$(/opt/node22/bin/node "/opt/meteor/tools/meteor.js" mongo --url findahouse.meteor.com <<<$password)
+cmd=$(meteor mongo --url findahouse)
 mongo_uri=$(echo $cmd | cut -f2 -d" ")
 mongo_db=`echo $mongo_uri | cut -d/ -f 4`
 
